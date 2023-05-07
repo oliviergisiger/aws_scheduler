103ce8b5782797f4ead22bbfbe32da0ebce3b791
oliviergisiger <oliviergisiger@bluewin.ch>
2023-05-07 11:19:16 +0200

    initial commit for redeployment to new ec2 instance

---
+++ restart_lab.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+curl 'https://labs.vocareum.com/util/vcput.php?a=startaws&stepid=1569028&version=0&mode=s&type=1&vockey=rUvDz4fZExx3whMjpx7S%2Bg%3D%3D' \
+  -H 'authority: labs.vocareum.com' \
+  -H 'accept: */*' \
+  -H 'accept-language: de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7' \
+  -H 'cookie: userid=2428323; usingLTI=1; vocuserid=2428323; myfolder=5bdbb6c1e99eed3754a27e43cbd1721f; currentcourse=vc_2_0_11992abdorg265_304; currentassignment=1569027; logintoken=5d88c860688dda17ec6dbf86d4387fad; tokenExpire=1683537132; usertoken=5d88c860688dda17ec6dbf86d4387fad; t2fausers=5d88c860688dda17ec6dbf86d4387fad' \
+  -H 'referer: https://labs.vocareum.com/main/main.php?m=clabide&mode=s&asnid=1569027&stepid=1569028&hideNavBar=1' \
+  -H 'sec-ch-ua: "Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "macOS"' \
+  -H 'sec-fetch-dest: empty' \
+  -H 'sec-fetch-mode: cors' \
+  -H 'sec-fetch-site: same-origin' \
+  -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36' \
+  -H 'x-requested-with: XMLHttpRequest' \
+  --compressed
