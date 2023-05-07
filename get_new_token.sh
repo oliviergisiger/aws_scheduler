64e4bce98d2abb953dd82c79d8dac962f06ce606
oliviergisiger <oliviergisiger@bluewin.ch>
2023-05-07 11:35:57 +0200

    variable added

--- get_new_token.sh
+++ get_new_token.sh
@@ -6,7 +6,7 @@ curl 'https://labs.vocareum.com/util/vcput.php?a=getaws&type=1&stepid=1569028&ve
   -H 'authority: labs.vocareum.com' \
   -H 'accept: */*' \
   -H 'accept-language: de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7' \
-  -H 'cookie: userid=2428323; usingLTI=1; vocuserid=2428323; myfolder=5bdbb6c1e99eed3754a27e43cbd1721f; currentcourse=vc_2_0_11992abdorg265_304; currentassignment=1569027; logintoken=5d88c860688dda17ec6dbf86d4387fad; tokenExpire=1683537132; usertoken=5d88c860688dda17ec6dbf86d4387fad; t2fausers=5d88c860688dda17ec6dbf86d4387fad' \
+  -H 'cookie: userid=2428323; usingLTI=1; vocuserid=2428323; myfolder=5bdbb6c1e99eed3754a27e43cbd1721f; currentcourse=vc_2_0_11992abdorg265_304; currentassignment=1569027; logintoken=5d88c860688dda17ec6dbf86d4387fad; tokenExpire="'$newExpirationTime'"; usertoken=5d88c860688dda17ec6dbf86d4387fad; t2fausers=5d88c860688dda17ec6dbf86d4387fad' \
   -H 'referer: https://labs.vocareum.com/main/main.php?m=clabide&mode=s&asnid=1569027&stepid=1569028&hideNavBar=1' \
   -H 'sec-ch-ua: "Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"' \
   -H 'sec-ch-ua-mobile: ?0' \
